44a6412850aee19fad258dc019613e2922b65d69
Leonardo <tattox26@hotmail.com>
2024-08-22 01:07:43 -0500

    save project

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('custom:task')->weekly()->mondays()->at('02:00');;
     }
 
     /**
